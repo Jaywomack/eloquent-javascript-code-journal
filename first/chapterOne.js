cd8f0eec8e156bc6af204a6f0a832b954aa94ef5
jaywomack <womackjw@hotmail.com>
2020-09-06 19:40:01 -0700

    calling it a night in the middle of the chapter 2 at breaking out of a loop

---
+++ first/chapterOne.js
@@ -0,0 +1,33 @@
+/***
+ * In chapter 1 values types and operators were discussed
+ *
+ * values are stored in memory as data
+ *
+ * Numbers
+ * numbers in javascript can be used to do arithmetic and many other things such as represent values for variables.
+ *
+ * Fractional numbers are written with a decimal and should not be trusted for accuracy
+ *
+ * exponents can be use with the 2.998e8 syntax which equals 2.998 *10^8
+ *
+ * All arithmetic can be used and follows the order of operations
+ *
+ * certain behaviors exists when javascript attempts to convert values from different types often these are unexpected outcomes
+ *
+ * SPECIAL NUMBERS
+ * infinity and -infinity infinity calculations are suspect and should be avoided
+ *
+ * strings use ' " " or `` the last is a template literal and allows for logic to be entered into the string `${4+4}`
+ *
+ * Unary Operators are - typeof and !
+ *
+ * Binary operators are -+ etc
+ *
+ * booleans are tru or false
+ *
+ * undefined and null are similar and usually interchangable while nan is a nonsensical value and undefined is a value yet to be defined and null is an empty value
+ *
+ * this or that this || that
+ *
+ * this then that this&&that
+ */
---
+++ first/chapterTwo.js
@@ -0,0 +1,134 @@
+// A fragment of code is an expression
+22;
+
+// statements that effect something are called side effects of the code
+
+// Binding or Variables
+let caught = 5 * 5;
+
+//example
+let luigisDebt = 140;
+luigisDebt = luigisDebt = 35;
+// console.log("Luigis debt:", luigisDebt);
+
+// The environment is the collection of bindings and their values
+// The environment is never empty because it contains bindings that are a part of the language standard.
+
+// Return values
+// console.log("The largest number is: ", Math.max(2, 4));
+
+// console.log("The smallest number plus 100 is:", Math.min(2, 4) + 100);
+
+// Control flow
+
+// let theNumber = Number(prompt("Pick a number")); // Prompt returns a string so we have to convert it to a number
+
+// console.log("Your number is the square root of " + theNumber * theNumber);
+
+// let theNumber = Number(prompt('Pick a number'));
+
+// IF ! Number is not a number(5)
+// confusing but means if it is a number
+// Unless the number is NaN do this
+// if (!Number.isNaN(theNumber)) {
+//   console.log('Your number is the square root of ' + theNumber * theNumber);
+// }
+
+// if (1 + 1 == 2) console.log("It's true");
+
+// let theNumber = Number(prompt('Pick a number'));
+// if (!Number.isNaN(theNumber)) {
+//   console.log('Your number is the square root of ' + theNumber * theNumber);
+// } else {
+//   console.log("Hey, why didn't you give me a number?");
+// }
+
+// let num = Number(prompt('Pick a number'));
+// if (num < 10) {
+//   console.log('Small');
+// } else if (num < 100) {
+//   console.log('Medium');
+// } else {
+//   console.log('Large');
+// }
+
+// WHILE and DO LOOPS
+
+// While a certain condition is met a while loop will continue to execute
+
+// let number = 0;
+// while (number <= 12) {
+//   console.log(number);
+//   number = number + 2;
+// }
+
+// Exponent Calculator
+// let result = 1;
+// let counter = 0;
+// while (counter < 10) {
+//   result = result * 2;
+//   counter = counter + 1;
+// }
+// // console.log(result);
+
+// // DO loops will always execute at least once and then start checking to see if the condition to continue the loop is present
+
+// // let yourName;
+// // do {
+// //   yourName = prompt('Who are you?');
+// // } while (!yourName); // Will make the loop continue until a non empty value is given
+// // // console.log(yourName);
+
+// // // indenting code
+// // if (false != true) {
+// //   console.log('That makes sense');
+// // }
+// // if (1 < 2) {
+// //   console.log('No surprise there');
+// // }
+
+// // FOR LOOPS
+
+// for (
+//   let computerProgramming = 0;
+//   computerProgramming <= 1000000;
+//   computerProgramming = computerProgramming + 100000
+// ) {
+//   console.log(computerProgramming);
+// }
+
+// for (let i = 0; i <= 20; i++) {
+//   console.log(i);
+// }
+
+// // Messing around trying to remember stuff i forgot
+
+// // Higher order functions
+// let carsArray = ['honda', 'ford', 'chevy', 'subaru', 'toyota'];
+// // For Loop iterating through an array
+// for (let i = 0; i < carsArray.length; i++) {
+//   console.log('The car in array 1 is: ', carsArray[i]);
+// }
+
+// // Mapping array to new array
+// let carsArray2 = [];
+// carsArray.map((car) => {
+//   carsArray2.push(`The make is ${car.toUpperCase()}`);
+// });
+// carsArray2.forEach((car) => {
+//   console.log(car);
+// });
+// // Using reduce()
+// let total = [];
+let numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// numbersArray.reduce((accumulator, item) => {
+//   total = accumulator + item;
+//   return total;
+// });
+
+// console.log(total);
+function isEven(num) {
+  return num % 2 === 0;
+}
+let evens = numbersArray.filter(isEven);
+console.log(evens);
